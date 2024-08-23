b9d73fc83a1f4c5632d08e496fa893b2f1ef6147
Sean MacAvaney <sean.macavaney@gmail.com>
2024-08-23 11:19:52 +0100

    package stuff

--- pyterrier_adaptive/__init__.py
+++ pyterrier_adaptive/__init__.py
@@ -1,2 +1,4 @@
+__version__ = '0.1.0'
+
 from .gar import GAR
 from .corpus_graph import CorpusGraph
--- setup.py
+++ setup.py
@@ -1,22 +1,40 @@
 from glob import glob
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def get_version(path):
+    for line in open(path):
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError(f"Unable to find __version__ in {path}")
+
+
+def get_requirements(path):
+    res = []
+    for line in open(path):
+        line = line.split('#')[0].strip()
+        if line:
+            res.append(line)
+    return res
 
 setuptools.setup(
-    name="pyterrier_adaptive",
-    version="0.0.1",
+    name="pyterrier-adaptive",
+    version=get_version('pyterrier_adaptive/__init__.py'),
     author="Sean MacAvaney",
     author_email="sean.macavaney@glasgow.ac.uk",
     description="PyTerrier implementation of Adaptive Re-Ranking using a Corpus Graph (CIKM 2022)",
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/terrierteam/pyterrier_adaptive",
     packages=setuptools.find_packages(include=['pyterrier_adaptive']),
     install_requires=list(open('requirements.txt')),
     classifiers=[],
     python_requires='>=3.6',
+    entry_points={
+        'pyterrier.artifact': [
+            'corpus_graph.np_topk = pyterrier_adaptive.corpus_graph:NpTopKCorpusGraph',
+        ],
+    },
     package_data={
         '': ['requirements.txt'],
     },
